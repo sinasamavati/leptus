dbe5e2f0002c34559510b00941335c906fe63fe0
Sina Samavati <sina.samv@gmail.com>
2013-09-20 15:49:53 +0430

    Add leptus_router:start_link/0, stop/0 find_mod/1

--- src/leptus_router.erl
+++ src/leptus_router.erl
@@ -1,15 +1,64 @@
 -module(leptus_router).
 
+-behaviour(gen_server).
+
+%% gen_server
+-export([init/1]).
+-export([handle_call/3]).
+-export([handle_cast/2]).
+-export([handle_info/2]).
+-export([terminate/2]).
+-export([code_change/3]).
+
+-export([start_link/0]).
+-export([stop/0]).
 -export([fetch_routes/1]).
 -export([dispatches/1]).
+-export([find_mod/1]).
 
--type routes() :: [{module(), [string()]}].
+-type req_route() :: string().
+-type routes() :: [{module(), [req_route()]}].
 
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+stop() ->
+    gen_server:cast(?MODULE, stop).
 
 -spec fetch_routes([module()]) -> routes().
 fetch_routes(Mods) ->
-    fetch_routes(Mods, []).
+    gen_server:call(?MODULE, {fetch_routes, Mods}).
 
+-spec find_mod(string()) -> {ok, module()} | {error, undefined}.
+find_mod(Route) ->
+    gen_server:call(?MODULE, {find_mod, Route}).
+
+%% gen_server
+init([]) ->
+    {ok, []}.
+
+handle_call({fetch_routes, Mods}, _From, _State) ->
+    NewState = fetch_routes(Mods, []),
+    {reply, NewState, NewState};
+
+handle_call({find_mod, Route}, _From, State) ->
+    {reply, find_mod(Route, State), State}.
+
+handle_cast(stop, State) ->
+    {stop, normal, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(normal, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+
+%% internal
 fetch_routes([], Acc) ->
     Acc;
 fetch_routes([Mod|T], Acc) ->
@@ -35,3 +84,20 @@ handle_routes([], Acc) ->
     Acc;
 handle_routes([Route|T], Acc) ->
     handle_routes(T, Acc ++ [{Route, leptus_resouce_handler, Route}]).
+
+-spec find_mod(req_route(), routes()) -> {ok, string()} | {error, undefined}.
+find_mod(Route, Routes) ->
+    Mod = orddict:fold(fun(K, V, Acc) ->
+                               case lists:member(Acc, V) of
+                                   true ->
+                                       K;
+                                   false ->
+                                       Acc
+                               end
+                       end, Route, Routes),
+
+    if Mod =/= Route ->
+            {ok, Mod};
+       true ->
+            {error, undefined}
+    end.
--- test/leptus_router_SUITE.erl
+++ test/leptus_router_SUITE.erl
@@ -1,12 +1,24 @@
 -module(leptus_router_SUITE).
 
 -export([all/0]).
+-export([init_per_testcase/2]).
+-export([end_per_testcase/2]).
+
 -export([fetch_routes/1]).
 -export([dispatches/1]).
+-export([find_mod/1]).
 
 
 all() ->
-    [fetch_routes, dispatches].
+    [fetch_routes, dispatches, find_mod].
+
+init_per_testcase(_, Config) ->
+    {ok, _} = leptus_router:start_link(),
+    Config.
+
+end_per_testcase(_, _Config) ->
+    leptus_router:stop().
+
 
 fetch_routes(_) ->
     [{leptus_routes1,
@@ -35,3 +47,14 @@ dispatches(_) ->
            ]
      }] = leptus_router:dispatches([leptus_routes1, leptus_routes2,
                                     leptus_routes3]).
+
+find_mod(_) ->
+    fetch_routes(0),
+    lists:foreach(fun(E) -> {ok, leptus_routes1} = leptus_router:find_mod(E) end,
+                  ["/", "/blah", "/hello/:name", "/some-url/to/some-path"]),
+    lists:foreach(fun(E) -> {ok, leptus_routes2} = leptus_router:find_mod(E) end,
+                  ["/something/:key", "/something/else"]),
+    lists:foreach(fun(E) -> {ok, leptus_routes3} = leptus_router:find_mod(E) end,
+                  ["/users/:id", "/users/:id/info"]),
+    lists:foreach(fun(E) -> {error, undefined} = leptus_router:find_mod(E) end,
+                  ["/1..", "2...", "3...."]).
