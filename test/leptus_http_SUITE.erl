871171363793eeebabb3825f9d8e38caf5ddb37a
Sina Samavati <sina.samv@gmail.com>
2013-09-20 17:56:31 +0430

    Update leptus_http_SUITE

--- test/leptus_http_SUITE.erl
+++ test/leptus_http_SUITE.erl
@@ -1,15 +1,21 @@
 -module(leptus_http_SUITE).
 
+-export([init_per_suite/1]).
 -export([all/0]).
--export([http_get/1]).
 
+-export([http_get/1]).
+-export([http_404/1]).
+
+
+init_per_suite(Config) ->
+    {ok, _} = leptus:start_http({modules, [leptus_http1]}),
+    Config.
 
 all() ->
-    [http_get].
+    [http_get, http_404].
+
 
 http_get(_) ->
-    {ok, _} = leptus:start_http({modules, [leptus_http1]}),
-
     {ok, 200, _, C1} = hackney:get("http://localhost:8080/"),
     {ok, <<"index">>, _} = hackney:body(C1),
 
@@ -18,3 +24,8 @@ http_get(_) ->
 
     {ok, 200, _, C3} = hackney:get("http://localhost:8080/hello/sina"),
     {ok, <<"hello, sina">>, _} = hackney:body(C3).
+
+http_404(_) ->
+    {ok, 404, _, _} = hackney:get("http://localhost:8080/asd"),
+    {ok, 404, _, _} = hackney:get("http://localhost:8080/asdf"),
+    {ok, 404, _, _} = hackney:get("http://localhost:8080/asdfg").
