a13ffbbcdbdc4c3432a1b27317f447140c663802
Sina Samavati <sina.samv@gmail.com>
2013-09-26 02:17:45 +0330

    Display status and content-length in console

--- src/leptus_hooks.erl
+++ src/leptus_hooks.erl
@@ -3,11 +3,21 @@
 -export([console_log/4]).
 
 
-console_log(_, _, _, Req) ->
-    %% [%Y-%m-%d %H:%M:%S] METHOD URL
+console_log(Status, Headers, _, Req) ->
+    %% [%Y-%m-%d %H:%M:%S]  METHOD URL  STATUS  CONTENT-LENGTH
     {{Year, Month, Day}, {Hour, Min, Sec}} = erlang:localtime(),
     {Method, _} = cowboy_req:method(Req),
     Uri = leptus_req:uri(Req),
-    io:format("[~w-~w-~w ~w:~w:~w] ~s ~s~n",
-              [Year, Month, Day, Hour, Min, Sec, Method, Uri]),
+    ContentLength = get_value(<<"content-length">>, Headers, 0),
+    io:format("[~w-~w-~w ~w:~w:~w]  ~s ~s  ~w  ~s~n",
+              [Year, Month, Day, Hour, Min, Sec, Method, Uri,
+               Status, ContentLength]),
     Req.
+
+
+%% internal
+get_value(Key, Proplist, Default) ->
+    case lists:keyfind(Key, 1, Proplist) of
+        {_, Value} -> Value;
+        _ -> Default
+    end.
