0856982783ca7503bbd1b587526b593047bfeaa9
Sina Samavati <sina.samv@gmail.com>
2013-09-25 23:00:15 +0330

    Use leptus_req:uri/1

--- src/leptus_hooks.erl
+++ src/leptus_hooks.erl
@@ -7,7 +7,7 @@ console_log(_, _, _, Req) ->
     %% [%Y-%m-%d %H:%M:%S] METHOD URL
     {{Year, Month, Day}, {Hour, Min, Sec}} = erlang:localtime(),
     {Method, _} = cowboy_req:method(Req),
-    {Url, _} = cowboy_req:url(Req),
+    Uri = leptus_req:uri(Req),
     io:format("[~w-~w-~w ~w:~w:~w] ~s ~s~n",
-              [Year, Month, Day, Hour, Min, Sec, Method, Url]),
+              [Year, Month, Day, Hour, Min, Sec, Method, Uri]),
     Req.
