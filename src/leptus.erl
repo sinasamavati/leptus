750f85ca9f7e7174dfa77a11e854eff49b9d6d0b
Sina Samavati <sina.samv@gmail.com>
2013-11-29 23:00:12 +0330

    Don't start reloader in start_http/1

--- src/leptus.erl
+++ src/leptus.erl
@@ -21,7 +21,6 @@ start_http(Handlers) ->
     ensure_started(crypto),
     ensure_started(ranch),
     ensure_started(cowboy),
-    reloader:start(),
 
     %% routes
     Paths = leptus_router:paths(Handlers),
