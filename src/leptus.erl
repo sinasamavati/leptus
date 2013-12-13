62fe98e648827ca8b9baa54ea485e25462425b52
Sina Samavati <sina.samv@gmail.com>
2013-12-13 23:10:46 +0330

    Add ensure_deps_started/0 (internal) & use leptus_config to deal with configs

--- src/leptus.erl
+++ src/leptus.erl
@@ -9,7 +9,6 @@
 -export([stop_http/0]).
 -export([upgrade/0]).
 -export([upgrade/1]).
--export([config/0]).
 
 -type handler() :: {module(), State::any()}.
 -type handlers() :: [handler()].
@@ -18,7 +17,9 @@
 
 -spec start_http() -> {ok, pid()} | {error, any()}.
 start_http() ->
-    start_http(get_value(handlers, config(), [])).
+    ensure_deps_started(),
+    ensure_started(leptus),
+    start_http(leptus_config:handlers(), []).
 
 -spec start_http(handlers()) -> {ok, pid()} | {error, any()}.
 start_http(Handlers) ->
@@ -26,20 +27,19 @@ start_http(Handlers) ->
 
 -spec start_http(handlers(), cowboy_protocol:opts()) -> {ok, pid()} | {error, any()}.
 start_http(Handlers, ProtoOpts) ->
-    %% ensure dependencies are started
-    ensure_started(crypto),
-    ensure_started(ranch),
-    ensure_started(cowboy),
+    ensure_deps_started(),
     ensure_started(leptus),
+
+    leptus_config:set(handlers, Handlers),
+    leptus_config:set(proto_opts, ProtoOpts),
 
     %% routes
     Paths = leptus_router:paths(Handlers),
     Dispatch = cowboy_router:compile([{'_', Paths}]),
 
     %% basic http configuration
-    Config = config(),
-    IP = ip_address(Config),
-    Port = http_port(Config),
+    IP = leptus_config:ip_addr(),
+    Port = leptus_config:port_num(),
 
     cowboy:start_http(
       leptus_http, 100, [{ip, IP}, {port, Port}],
@@ -55,9 +55,9 @@ stop_http() ->
 
 -spec upgrade() -> ok.
 upgrade() ->
-    upgrade(get_value(handlers, config(), [])).
+    upgrade(leptus_config:handlers()).
 
--spec upgrade([{module(), State :: any()}]) -> ok.
+-spec upgrade(handlers()) -> ok.
 upgrade(Handlers) ->
     Paths = leptus_router:paths(Handlers),
     cowboy:set_env(leptus_http, dispatch, cowboy_router:compile([{'_', Paths}])).
@@ -72,43 +72,8 @@ ensure_started(App) ->
             ok
     end.
 
-%% get IP address to bind to
-ip_address(Config) ->
-    Default = {127, 0, 0, 1},
-    case get_value(http, Config, Default) of
-        Default ->
-            Default;
-        Http ->
-            case inet_parse:address(get_value(ip, Http, Default)) of
-                {ok, IP} ->
-                    IP;
-                {error, _} ->
-                    Default
-            end
-    end.
-
-%% get http port to listen on
-http_port(Config) ->
-    Default = 8080,
-    case get_value(http, Config, Default) of
-        Default ->
-            Default;
-        Http ->
-            get_value(port, Http, Default)
-    end.
-
-%% read priv/leptus.config file
-config() ->
-    {ok, Cwd} = file:get_cwd(),
-    case file:consult(filename:join([Cwd, "priv", "leptus.config"])) of
-        {error, _} ->
-            [];
-        {ok, Terms} ->
-            Terms
-    end.
-
-get_value(Key, Proplist, Default) ->
-    case lists:keyfind(Key, 1, Proplist) of
-        {_, Value} -> Value;
-        _ -> Default
-    end.
+%% ensure dependencies are started
+ensure_deps_started() ->
+    ensure_started(crypto),
+    ensure_started(ranch),
+    ensure_started(cowboy).
