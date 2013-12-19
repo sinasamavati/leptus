efd684e169bf497b7ed975f162437b28868479b4
Sina Samavati <sina.samv@gmail.com>
2013-12-20 02:13:16 +0330

    Remove leptus:start_http/0 & update leptus:start_http/2

--- src/leptus.erl
+++ src/leptus.erl
@@ -3,7 +3,6 @@
 -module(leptus).
 -author("Sina Samavati <sina.samv@gmail.com>").
 
--export([start_http/0]).
 -export([start_http/1]).
 -export([start_http/2]).
 -export([stop_http/0]).
@@ -14,27 +13,36 @@
 -type handlers() :: [handler()].
 -export_type([handlers/0]).
 
+-type option() :: {priv_dir, atom()}.
+-type options() :: [option()].
 
--spec start_http() -> {ok, pid()} | {error, any()}.
-start_http() ->
-    ensure_deps_started(),
-    ensure_started(leptus),
-    start_http(leptus_config:handlers(), []).
 
 -spec start_http(handlers()) -> {ok, pid()} | {error, any()}.
 start_http(Handlers) ->
     start_http(Handlers, []).
 
--spec start_http(handlers(), cowboy_protocol:opts()) -> {ok, pid()} | {error, any()}.
-start_http(Handlers, ProtoOpts) ->
+-spec start_http(handlers(), options()) -> {ok, pid()} | {error, any()}.
+start_http(Handlers, Options) ->
     ensure_deps_started(),
     ensure_started(leptus),
 
     leptus_config:set(handlers, Handlers),
-    leptus_config:set(proto_opts, ProtoOpts),
+    case get_value(priv_dir, Options) of
+        undefined ->
+            ok;
+        App ->
+            %% set priv_dir & initialize 'handlers' and 'http' k/v
+            leptus_config:set(priv_dir, App),
+            %% read leptus.config and insert configurations to ets
+            Conf = leptus_config:config_file(App),
+            Http = get_value(http, Conf),
+            Handlers1 = get_value(handlers, Conf, Handlers),
+            leptus_config:set(http, Http),
+            leptus_config:set(handlers, Handlers1)
+    end,
 
     %% routes
-    Paths = leptus_router:paths(Handlers),
+    Paths = leptus_router:paths(leptus_config:handlers()),
     Dispatch = cowboy_router:compile([{'_', Paths}]),
 
     %% basic http configuration
@@ -46,7 +54,7 @@ start_http(Handlers, ProtoOpts) ->
       [
        {env, [{dispatch, Dispatch}]},
        {onresponse, fun leptus_hooks:console_log/4}
-      ] ++ ProtoOpts
+      ]
      ).
 
 -spec stop_http() -> ok | {error, not_found}.
@@ -77,3 +85,12 @@ ensure_deps_started() ->
     ensure_started(crypto),
     ensure_started(ranch),
     ensure_started(cowboy).
+
+get_value(Key, Opts) ->
+    get_value(Key, Opts, undefined).
+
+get_value(Key, Opts, Default) ->
+    case lists:keyfind(Key, 1, Opts) of
+        {_, V} -> V;
+        _ -> Default
+    end.
