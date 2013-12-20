c00ce81792b20cc80f95f5787470259767271667
Sina Samavati <sina.samv@gmail.com>
2013-12-20 14:57:58 +0330

    Add leptus_json:parser/0 & fix rebar.config.script to use the exising json parser

--- src/leptus_json.erl
+++ src/leptus_json.erl
@@ -2,13 +2,16 @@
 
 -export([encode/1]).
 -export([decode/1]).
+-export([parser/0]).
 
 -ifdef(USE_JSX).
 -define(ENCODE(Term), jsx:encode(Term)).
 -define(DECODE(Term), jsx:decode(Term)).
+-define(PARSER, jsx).
 -else.
 -define(ENCODE(Term), jiffy:encode({Term})).
 -define(DECODE(Term), element(1, jiffy:decode(Term))).
+-define(PARSER, jiffy).
 -endif.
 
 
@@ -17,3 +20,6 @@ encode(Term) ->
 
 decode(Term) ->
     ?DECODE(Term).
+
+parser() ->
+    ?PARSER.
