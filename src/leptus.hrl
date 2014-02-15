b79f910c2fb3563869b6c085693d0e162a250089
Sina Samavati <sina.samv@gmail.com>
2014-02-15 06:45:27 +0330

    Update src/leptus.hrl

--- src/leptus.hrl
+++ src/leptus.hrl
@@ -1,4 +1,10 @@
 %% This file is part of leptus, and released under the MIT license.
 %% See LICENSE for more information.
 
--record(ctx, {route, handler, handler_state}).
+-record(ctx,
+        {
+          route :: cowboy_router:route_match(),
+          handler:: module(),
+          handler_state :: any()
+        }).
+-type ctx() :: #ctx{}.
