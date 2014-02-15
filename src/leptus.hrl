624b1ff0e2384215a685848655a0eeaa742ad2ea
Sina Samavati <sina.samv@gmail.com>
2014-02-16 01:07:05 +0330

    Include a number of types into src/leptus.hrl

--- src/leptus.hrl
+++ src/leptus.hrl
@@ -1,10 +1,14 @@
 %% This file is part of leptus, and released under the MIT license.
 %% See LICENSE for more information.
 
+-type route() :: '_' | iodata().
+-type handler() :: module().
+-type handler_state() :: any().
+
 -record(ctx,
         {
-          route :: cowboy_router:route_match(),
-          handler:: module(),
+          route :: route(),
+          handler :: module(),
           handler_state :: any()
         }).
 -type ctx() :: #ctx{}.
