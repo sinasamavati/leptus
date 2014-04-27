6a9a22d25011b1db4579a70427cfaf23bb421cc2
Sina Samavati <sina.samv@gmail.com>
2014-04-27 11:56:59 +0430

    Update leptus_pt module to not use process dictionary for gathering routes when parse transforming

--- src/leptus_pt.erl
+++ src/leptus_pt.erl
@@ -24,109 +24,77 @@
 
 -export([parse_transform/2]).
 
-
 parse_transform(AST, _Options) ->
-    put(routes, []),
-    walk_ast(AST, []).
+    %% AST, Acc, Routes :: [{Route, AllowedMethod :: binary()}]
+    walk_ast(AST, [], []).
 
+%% -----------------------------------------------------------------------------
 %% internal
-walk_ast([], Acc) ->
-    add_allowed_methods_fun(add_routes_fun(Acc));
-walk_ast([{attribute, _, export, _}=H|T], Acc) ->
-    case is_transformed(export_funcs) of
-        true ->
-            walk_ast(T, Acc ++ [H]);
-        _ ->
-            %% export routes/0 and allowed_methods/1
-            walk_ast(T, Acc ++ [export_funcs(H)])
-    end;
-walk_ast([{function, _, Method, 3, _}=H|T], Acc)
-  when Method =:= get; Method =:= put; Method =:= post; Method =:= delete ->
-    case is_transformed(Method) of
-        true ->
-            walk_ast(T, Acc ++ [H]);
-        _ ->
-            %% collect routes
-            walk_ast(T, Acc ++ [check_clauses(H)])
-    end;
-walk_ast([H|T], Acc) ->
-    walk_ast(T, Acc ++ [H]).
-
+%% -----------------------------------------------------------------------------
+walk_ast([], Acc, _) ->
+    Acc;
+%% -----------------------------------------------------------------------------
 %% export routes/0 and allowed_methods/1
-export_funcs({attribute, L, export, Funcs}) ->
-    transformed(export_funcs),
-    {attribute, L, export, Funcs ++ [{routes, 0}, {allowed_methods, 1}]}.
+%% -----------------------------------------------------------------------------
+walk_ast([{attribute, L, module, _}=Form|Rest], Acc, Routes) ->
+    ExportForm = {attribute, L, export, [{routes, 0}, {allowed_methods, 1}]},
+    walk_ast(Rest, Acc ++ [Form] ++ [ExportForm], Routes);
+%% -----------------------------------------------------------------------------
+%% collect routes
+%% -----------------------------------------------------------------------------
+walk_ast([{function, _, Method, 3, _}=Form|Rest], Acc, Routes)
+  when Method =:= get; Method =:= put; Method =:= post; Method =:= delete ->
+    Routes1 = check_clauses(Form),
+    walk_ast(Rest, Acc ++ [Form], Routes ++ Routes1);
+%% -----------------------------------------------------------------------------
+%% add routes/0 to the module
+%% i.e. routes() -> [Route]
+%% -----------------------------------------------------------------------------
+%% add allowed_methods/1 to the module
+%% e.g. allowed_methods("/") -> [<<"GET">>, <<"PUT">>]
+%% -----------------------------------------------------------------------------
+walk_ast([{eof, L}=Form|Rest], Acc, Routes) ->
+    F = fun({Route, Method}, AccIn) ->
+                case lists:keyfind(Route, 1, AccIn) of
+                    {Route, Methods} ->
+                        lists:keystore(Route, 1, AccIn,
+                                       {Route, [Method|Methods]});
+                    _ ->
+                        [{Route, [Method]}|AccIn]
+                end
+        end,
+    %% [{string(), [binary()]}]
+    RoutesNMethods = lists:usort(lists:foldr(F, [], Routes)),
 
+    %% [string()]
+    Routes1 = [Route || {Route, _} <- RoutesNMethods],
+
+    %% routes() -> [Route]
+    Routes0Fun = {function, L, routes, 0,
+                  [{clause, L, [], [],
+                    [erl_parse:abstract(Routes1, [{line, L}])]}]},
+
+    %% allowed_methods(Route) -> [binary()]
+    AllowedMethods1Fun = {function, L, allowed_methods, 1,
+                          [{clause, L, [{string, L, Route}], [],
+                            [erl_parse:abstract(Methods, [{line, L}])]}
+                           || {Route, Methods} <- RoutesNMethods]},
+    walk_ast(Rest, Acc ++ [Routes0Fun|[AllowedMethods1Fun]] ++ [Form], Routes);
+walk_ast([Form|Rest], Acc, Routes) ->
+    walk_ast(Rest, Acc ++ [Form], Routes).
+
+%% -----------------------------------------------------------------------------
 %% check functions' head
-check_clauses({function, _, Method, 3, Clause}=H) ->
+%% -----------------------------------------------------------------------------
+check_clauses({function, _, Method, 3, Clauses}) ->
     %% collect routes
-    F = fun({clause, _, E, _, _}=Token) ->
+    F = fun({clause, _, E, _, _}) ->
                 %% e.g. get("/", _Req, _State)
                 {string, _, Route} = hd(E),
-                add_route(Route, Method),
-                Token
+                %% e.g. {"/", <<"GET">>}
+                {Route, http_method(Method)}
         end,
-    lists:foreach(F, Clause),
-    transformed(Method),
-    H.
-
-%% append a route to the 'routes' key
-add_route(Route, Method) ->
-    MethodsList = case get(Route) of
-                      undefined ->
-                          [];
-                      Else ->
-                          Else
-                  end,
-    put(Route, MethodsList ++ [http_method(Method)]),
-    put(routes, get(routes) ++ [Route]).
-
-%% add routes/0 to the module
-%% i.e. routes() -> [Route].
-add_routes_fun(AST) ->
-    {eof, L} = lists:keyfind(eof, 1, AST),
-
-    %% remove duplicate elements
-    Routes = lists:usort(get(routes)),
-
-    put(routes, Routes),
-    AST1 = AST -- [{eof, L}],
-    AST1 ++ [
-             {function, L, routes, 0,
-              [
-               {clause, L, [], [],
-                [erl_parse:abstract(Routes, [{line, L}])]
-               }
-              ]
-             },
-             {eof, L}
-            ].
-
-%% add allowed_methods/1 to the module
-%% e.g allowed_methods("/") -> [<<"GET">>, <<"PUT">>].
-add_allowed_methods_fun(AST) ->
-    {eof, L} = lists:keyfind(eof, 1, AST),
-    Routes = get(routes),
-    AST1 = AST -- [{eof, L}],
-
-    AST1 ++ [
-             {function, L, allowed_methods, 1,
-              [
-               {clause, L, [{string, L, R}], [],
-                [erl_parse:abstract(get(R), [{line, L}])]
-               } || R <- Routes
-              ]
-             },
-             {eof, L}
-            ].
-
-%% give X the value 'true'
-transformed(X) ->
-    put(X, true).
-
-%% check if X has a value
-is_transformed(X) ->
-    get(X) =/= undefined.
+    [F(Clause) || Clause <- Clauses].
 
 http_method(get) -> <<"GET">>;
 http_method(put) -> <<"PUT">>;
