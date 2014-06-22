674e48e6384d415825132413809ce33e70f9358a
Sina Samavati <sina.samv@gmail.com>
2014-06-22 13:18:59 +0430

    Ask for a new access log file once a day

--- src/leptus_access_log.erl
+++ src/leptus_access_log.erl
@@ -36,15 +36,17 @@
 -define(FILENAME_SUFFIX, "-access.log").
 -define(LOG_FORMAT, "~h ~l ~u ~t \"~r\" ~s ~B \"~{referer}\" \"~{user-agent}\"").
 
+-record(state, {dir, io_dev, log_fmt, tref}).
+
 init({Dir, LogFmt}) ->
-    Filename = filename(Dir),
-    filelib:ensure_dir(Filename),
-    {ok, IoDev} = file:open(Filename, [append]),
-    {ok, {IoDev, LogFmt}};
+    %% ask for a new file once a day
+    {ok, TRef} = timer:send_interval(millsec_til_tomorrow(), open_new_file),
+    {ok, IoDev} = open_file(Dir),
+    {ok, #state{dir = Dir, io_dev = IoDev, log_fmt = LogFmt, tref = TRef}};
 init(Dir) ->
     init({Dir, ?LOG_FORMAT}).
 
-handle_event({access_log, LogData}, State={IoDev, LogFmt}) ->
+handle_event({access_log, LogData}, State=#state{io_dev=IoDev, log_fmt=LogFmt}) ->
     Log = leptus_logger:format(LogFmt, LogData),
     file:write(IoDev, [Log, $\n]),
     {ok, State};
@@ -54,17 +56,43 @@ handle_event(_, State) ->
 handle_call(_Request, State) ->
     {ok, State}.
 
+handle_info(open_new_file, State=#state{dir=Dir, io_dev=IoDev}) ->
+    file:close(IoDev),
+    {ok, IoDev1} = open_file(Dir),
+    {ok, State#state{io_dev = IoDev1}};
 handle_info(_Info, State) ->
     {ok, State}.
 
-terminate(_Args, {IoDev, _}) ->
+terminate(_Args, #state{io_dev=IoDev, tref=TRef}) ->
     file:close(IoDev),
+    timer:cancel(TRef),
     ok.
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
+%% -----------------------------------------------------------------------------
+%% make filename. e.g. "Dir/2014_06_22-access.log"
+%% -----------------------------------------------------------------------------
 filename(Dir) ->
     {{Y, M, D}, _} = erlang:localtime(),
     filename:join(Dir, io_lib:format("~w_~2..0w_~2..0w~s",
                                      [Y, M, D, ?FILENAME_SUFFIX])).
+
+%% -----------------------------------------------------------------------------
+%% take directory, make a filename, and open file
+%% -----------------------------------------------------------------------------
+open_file(Dir) ->
+    Filename = filename(Dir),
+    filelib:ensure_dir(Filename),
+    file:open(Filename, [append, raw]).
+
+%% -----------------------------------------------------------------------------
+%% milliseconds left until a new day arrives
+%% -----------------------------------------------------------------------------
+millsec_til_tomorrow() ->
+    {_, {CH, CM, CS}} = erlang:localtime(),
+    H = (23 - CH) * (60 * 60) * 1000,
+    M = (59 - CM) * (60 * 1000),
+    S = (59 - CS) * 1000,
+    (H + M) + S.
